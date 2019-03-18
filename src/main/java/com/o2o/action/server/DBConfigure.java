fea9ee7006b072072cd3ee76faaa565b6fd0d200
snopy.lee <1560656>
2019-03-18 14:11:58 +0900

    Data Source 수정

--- src/main/java/com/o2o/action/server/DBConfigure.java
+++ src/main/java/com/o2o/action/server/DBConfigure.java
@@ -1,6 +1,7 @@
 package com.o2o.action.server;
 
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DBConfigure {
-    @ConfigurationProperties(prefix = "spring.datasource1")
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource1")
+    public DataSourceProperties dataSource1Prop() {
+        return new DataSourceProperties();
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource1.configuration")
     @Bean
     @Primary
     public DataSource dataSource1(){
-        return DataSourceBuilder
-                .create()
-                .build();
+        return dataSource1Prop().initializeDataSourceBuilder().build();
     }
 }
-
