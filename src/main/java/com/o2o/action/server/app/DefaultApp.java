3d6ad054e5871bb780c6307927713f3d2849c363
JungHyo <wjdgy0215@o2o.kr>
2019-03-26 10:13:30 +0900

    고객센터 이미지 넣기

--- src/main/java/com/o2o/action/server/app/DefaultApp.java
+++ src/main/java/com/o2o/action/server/app/DefaultApp.java
@@ -1,12 +1,12 @@
 package com.o2o.action.server.app;
 
-import com.google.actions.api.ActionRequest;
-import com.google.actions.api.ActionResponse;
-import com.google.actions.api.DialogflowApp;
-import com.google.actions.api.ForIntent;
+import com.beust.jcommander.Parameter;
+import com.google.actions.api.*;
 import com.google.actions.api.response.ResponseBuilder;
+import com.google.actions.api.response.helperintent.Confirmation;
 import com.google.actions.api.response.helperintent.SelectionCarousel;
 import com.google.api.client.util.Lists;
+import com.google.api.client.util.Value;
 import com.google.api.services.actions_fulfillment.v2.model.BasicCard;
 import com.google.api.services.actions_fulfillment.v2.model.CarouselSelectCarouselItem;
 import com.google.api.services.actions_fulfillment.v2.model.Image;
@@ -28,10 +28,15 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
+import static java.awt.Color.black;
+
 public class DefaultApp extends DialogflowApp {
     private CategoryRepository categoryRepository;
     private ChannelRepository channelRepository;
     private ScheduleRepository scheduleRepository;
+    Boolean IsCable = false;
+    Boolean IsArs = false;
+    int cCount[] = {0};
 
 
     public void setCategoryRepository(CategoryRepository categoryRepository) {
@@ -46,66 +51,226 @@ public class DefaultApp extends DialogflowApp {
         this.scheduleRepository = scheduleRepository;
     }
 
-    @ForIntent("Channel")
-    public ActionResponse processChannel(ActionRequest request) throws ExecutionException, InterruptedException {
-        ResponseBuilder responseBuilder = getResponseBuilder(request);
-        List<String> suggestions = new ArrayList<String>();
-        suggestions.add("채널로 이동");
+    /*
+        @ForIntent("Channel")
+        public ActionResponse processChannel(ActionRequest request) throws ExecutionException, InterruptedException {
+            ResponseBuilder responseBuilder = getResponseBuilder(request);
+            List<String> suggestions = new ArrayList<String>();
+            suggestions.add("채널로 이동");
 
-        QueryResult qr = request.getWebhookRequest().getQueryResult();
+            QueryResult qr = request.getWebhookRequest().getQueryResult();
 
-        String eChannel = null;
+            String eChannel = null;
 
-        if (qr != null) {
-            Map<String, Object> params = qr.getParameters();
-            eChannel = (String) params.get("Ent_channelname");
-        }
+            if (qr != null) {
+                Map<String, Object> params = qr.getParameters();
+                eChannel = (String) params.get("Ent_channelname");
+            }
 
-        System.out.println(eChannel);
+            System.out.println(eChannel);
 
-        if (eChannel == null) {
-            processError(responseBuilder, suggestions, "방송 정보가 없습니다.");
+            if (eChannel == null) {
+                processError(responseBuilder, suggestions, "방송 정보가 없습니다.");
+                return responseBuilder.build();
+            }
+
+            List<Channel> channels = Lists.newArrayList(channelRepository.findByChName(eChannel));
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+            String tmpStr = formatter.format(new Date()) + "00";
+
+            long curTime = Long.parseLong(tmpStr) + 90000;
+            System.out.println(curTime);
+
+            for (Channel channel : channels) {
+                List<Schedule> schedules = scheduleRepository.findByChannelAndStartTimeLessThanEqualAndEndTimeGreaterThan(channel, curTime, curTime);
+                for (Schedule schedule : schedules) {
+                    responseBuilder.add(schedule.getName());
+                }
+            }
             return responseBuilder.build();
         }
 
-        List<Channel> channels = Lists.newArrayList(channelRepository.findByChName(eChannel));
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
-        String tmpStr = formatter.format(new Date()) + "00";
 
-        long curTime = Long.parseLong(tmpStr) + 90000;
-        System.out.println(curTime);
+        @ForIntent("Shopping")
+        public ActionResponse processShop(ActionRequest request) throws ExecutionException, InterruptedException {
+            ResponseBuilder responseBuilder = getResponseBuilder(request);
+            List<String> suggestions = new ArrayList<String>();
+            suggestions.add("쇼핑으로 이동");
 
-        for (Channel channel : channels) {
-            List<Schedule> schedules = scheduleRepository.findByChannelAndStartTimeLessThanEqualAndEndTimeGreaterThan(channel, curTime, curTime);
-            for (Schedule schedule : schedules) {
-                responseBuilder.add(schedule.getName());
+            Category root = null;
+            List<Category> roots = categoryRepository.findByKeycodeOrderByDispOrderAsc(DBInit.KEYCODE_SHOPPING_ROOT);
+
+            if (roots != null && roots.size() > 0) {
+                root = roots.get(0);
+                System.out.println(root.getId() + "," + root.getKeycode());
+            }
+            if (root != null)
+                processRootCategories(responseBuilder, suggestions, root);
+            else
+                processError(responseBuilder, suggestions, "쇼핑 정보를 읽어올 수 없습니다.");
+
+            return responseBuilder.build();
+        }
+    */
+
+    @ForIntent("support-find.symptom")
+    public ActionResponse processFindSymptom(ActionRequest request) throws ExecutionException, InterruptedException {
+        ResponseBuilder rb = getResponseBuilder(request);
+        List<String> suggestions = new ArrayList<String>();
+
+        String symptom = null;
+        Object oSymptom = (Object) request.getParameter("symptom");
+
+        System.out.println("oSymptom : [" + oSymptom + "]");
+
+        // 증상 파악을 위해서 단계별로 접근 할 수 있어야 한다.
+        // 화면이 들어올 경우 깜박임까지 합쳐서 다음 단계로 넘어 가야 한다.
+        if (oSymptom != null && oSymptom instanceof String) {
+            symptom = (String) oSymptom;
+        }
+
+        if (symptom == null || symptom.length() <= 0) {
+            rb.add("화면이 어떻게 이상한가요?");
+            suggestions.add("화면이 깜박여요");
+            suggestions.add("화면이 안나와요");
+        } else {
+            //check status로 유도
+            if (symptom.equalsIgnoreCase("sym1")) {
+                rb.add("TV 케이블 연결은 어떻게 되어 있나요?");
+
+                BasicCard basicCard = new BasicCard()
+                        .setTitle("HDMI로 연결")
+                        .setFormattedText("HDMI 케이블");
+
+                basicCard.setImage(new Image().setUrl("https://actions.o2o.kr/content/cable_hdmi.jpg").setAccessibilityText("hdmi케이블이미지"));
+                rb.add(basicCard);
+
+                suggestions.add("HDMI로 연결");
+                suggestions.add("컴포넌트로 연결");
+            }
+            if (symptom.equalsIgnoreCase("sym2")) {
+
             }
         }
-        return responseBuilder.build();
+
+        rb.addSuggestions(suggestions.toArray(new String[suggestions.size()]));
+
+        return rb.build();
     }
 
-
-    @ForIntent("Shopping")
-    public ActionResponse processShop(ActionRequest request) throws ExecutionException, InterruptedException {
-        ResponseBuilder responseBuilder = getResponseBuilder(request);
+    @ForIntent("support-check.status")
+    public ActionResponse processCheckStatus(ActionRequest request) throws ExecutionException, InterruptedException {
+        ResponseBuilder rb = getResponseBuilder(request);
         List<String> suggestions = new ArrayList<String>();
-        suggestions.add("쇼핑으로 이동");
 
-        Category root = null;
-        List<Category> roots = categoryRepository.findByKeycodeOrderByDispOrderAsc(DBInit.KEYCODE_SHOPPING_ROOT);
+        ActionContext context = request.getContext("support-find-symptom");
+        Map<String, Object> SymptomParas = context.getParameters();
 
-        if (roots != null && roots.size() > 0) {
-            root = roots.get(0);
-            System.out.println(root.getId() + "," + root.getKeycode());
+        String symptom = null;
+        Object oSymptom = (Object) SymptomParas.get("symptom");
+        String connectionType = null;
+        Object oConnectionType = (Object) request.getParameter("connectionType");
+
+        System.out.println(oSymptom);
+        System.out.println(oConnectionType);
+
+        if (oSymptom != null && oSymptom instanceof String) {
+            symptom = (String) oSymptom;
         }
-        if (root != null)
-            processRootCategories(responseBuilder, suggestions, root);
-        else
-            processError(responseBuilder, suggestions, "쇼핑 정보를 읽어올 수 없습니다.");
+        if (oConnectionType != null && oConnectionType instanceof String) {
+            connectionType = (String) oSymptom;
+        }
 
-        return responseBuilder.build();
+        //resolution으로 유도
+        rb.add("알겠습니다. 다음과 같이 시도해 보세요. 케이블 연결을 다시 한번 확인해 보시겠습니까?");
+        suggestions.add("잘되요");
+        suggestions.add("여전히 이상해요");
+
+        Map<String, Object> data = rb.getConversationData();
+        data.remove("solution");
+        data.put("solution", "1");
+
+        rb.addSuggestions(suggestions.toArray(new String[suggestions.size()]));
+
+        return rb.build();
     }
 
+    @ForIntent("support-resolution.notwork")
+    public ActionResponse processResolutionNotwork(ActionRequest request) throws ExecutionException, InterruptedException {
+        ResponseBuilder rb = getResponseBuilder(request);
+        List<String> suggestions = new ArrayList<String>();
+        List<CarouselSelectCarouselItem> items = new ArrayList<>();
+        CarouselSelectCarouselItem item;
+
+        ActionContext context = request.getContext("support-find-symptom");
+        Map<String, Object> SymptomParas = context.getParameters();
+
+        String symptom = null;
+        Object oSymptom = (Object) SymptomParas.get("symptom");
+        String connectionType = null;
+        Object oConnectionType = (Object) SymptomParas.get("connectionType");
+
+        Map<String, Object> data = rb.getConversationData();
+        int solution = 0;
+        Object oSolution = data.get("solution");
+
+        System.out.println(oSymptom);
+        System.out.println(oConnectionType);
+        System.out.println(oSolution);
+
+        if (oSymptom != null && oSymptom instanceof String) {
+            symptom = (String) oSymptom;
+        }
+        if (oConnectionType != null && oConnectionType instanceof String) {
+            connectionType = (String) oSymptom;
+        }
+        if (oSolution != null && oSolution instanceof String) {
+            solution = Integer.parseInt((String) oSolution);
+        }
+
+        if (solution == 1) {
+            data.put("solution", "2");
+            rb.add("알겠습니다. TV 전원을 다시 한번 껏다 켜보시겠어요?");
+            suggestions.add("잘되요");
+            suggestions.add("여전히 이상해요");
+        } else if (solution == 2) {
+            data.put("solution", "3");
+            rb.add("알겠습니다. RF 케이블 연결 상태를 확인해 주시겠어요?");
+            suggestions.add("잘되요");
+            suggestions.add("여전히 이상해요");
+            
+        } else {
+            data.put("solution", "-1");
+            rb.add(new Confirmation().setConfirmationText("원격진단 이나 콜센터 연결 방법을 알려 드릴까요?"));
+        }
+        //resolution으로 유도
+        rb.addSuggestions(suggestions.toArray(new String[suggestions.size()]));
+
+        return rb.build();
+    }
+
+    @ForIntent("support-resolution.notwork-call.confirm")
+    public ActionResponse processResolutionNotworkCallConfirm(ActionRequest request) throws ExecutionException, InterruptedException {
+        ResponseBuilder rb = getResponseBuilder(request);
+        List<String> suggestions = new ArrayList<String>();
+
+        boolean userConfirmation = request.getUserConfirmation();
+
+        if (userConfirmation) {
+            rb.add(new ActionContext("support-ascontrol-followup", 5));
+            rb.add(new Confirmation().setConfirmationText("콜센터는 1588-0032 로 전화해 주시면 됩니다." +
+                    "원격진단은 전화전에 스마트카드 번호를 확인해 주세요." +
+                    "스마트카드 확인 방법을 알려 드릴까요?"));
+
+            rb.addSuggestions(suggestions.toArray(new String[suggestions.size()]));
+
+        } else {
+            rb.add("도움이 못되어서 죄송합니다. 다음에 다시 만나요.");
+        }
+        return rb.build();
+    }
+}
+/*
     @ForIntent("Support")
     public ActionResponse processSupport(ActionRequest request) throws ExecutionException, InterruptedException {
         ResponseBuilder responseBuilder = getResponseBuilder(request);
@@ -578,3 +743,4 @@ public class DefaultApp extends DialogflowApp {
         return responseBuilder.build();
     }
 }
+*/
