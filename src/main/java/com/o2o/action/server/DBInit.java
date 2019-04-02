62b61bb0126ba6f23a36c282840ec4cfe79c6615
cym7239 <47097489+cym7239@users.noreply.github.com>
2019-04-02 17:27:31 +0900

    Shopping APP

--- src/main/java/com/o2o/action/server/DBInit.java
+++ src/main/java/com/o2o/action/server/DBInit.java
@@ -14,6 +14,7 @@ public class DBInit {
     public static final String KEYCODE_CUSTOMER_SERVICE_ROOT = "cserviceroot";
     public static final String KEYCODE_SHOPPING_ROOT = "shoppingroot";
     public static final String KEYCODE_BRODCAST_CHANNEL = "channelroot";
+    public static final String KEYCODE_SHOPPING_GS = "shop.gs";
    //public static final String KEYCODE_XIAOMI_SHOP_ROOT = "xaiomishop";
 
     @Autowired
