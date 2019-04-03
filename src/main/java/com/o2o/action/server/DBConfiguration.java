5970d5b0c5420a3d4e32da71a3f850ffb9ade8d0
snopy.lee <snopy.lee@o2o.kr>
2019-04-03 10:47:40 +0900

    불필요한 코드 삭제

--- src/main/java/com/o2o/action/server/DBConfiguration.java
+++ src/main/java/com/o2o/action/server/DBConfiguration.java
@@ -1,63 +1,26 @@
 package com.o2o.action.server;
 
+import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
-/*
 @Configuration
 public class DBConfiguration {
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.cym")
-    public DataSourceProperties dataSourcecymProp() {
-        return new DataSourceProperties();
-    }
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource1")
+	public DataSourceProperties dataSource1Prop() {
+		return new DataSourceProperties();
+	}
 
-    @ConfigurationProperties(prefix = "spring.datasource.cym.configuration")
-    @Bean
-    @Primary
-    public DataSource dataSourceCym(){
-        return dataSourcecymProp().initializeDataSourceBuilder().build();
-    }
-}
-*/
-/*
-@Configuration
-public class DBConfiguration {
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.no1")
-    public DataSourceProperties dataSourceno1Prop() {
-        return new DataSourceProperties();
-    }
-
-    @ConfigurationProperties(prefix = "spring.datasource.no1.configuration")
-    @Bean
-    @Primary
-    public DataSource dataSourceNo1(){
-        return dataSourceno1Prop().initializeDataSourceBuilder().build();
-    }
-}*/
-
-@Configuration
-public class DBConfiguration {
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource1")
-    public DataSourceProperties dataSource1Prop() {
-        return new DataSourceProperties();
-    }
-
-    @ConfigurationProperties(prefix = "spring.datasource1.configuration")
-    @Bean
-    @Primary
-    public DataSource dataSource1(){
-        return dataSource1Prop().initializeDataSourceBuilder().build();
-    }
+	@ConfigurationProperties(prefix = "spring.datasource1.configuration")
+	@Bean
+	@Primary
+	public DataSource dataSource1() {
+		return dataSource1Prop().initializeDataSourceBuilder().build();
+	}
 }
